ec1bca5940df8cddd1b1cc901cfee7b5617dbc97
Pawan Kumar <pawankumar@Drmacs-MacBook-Air.local>
2019-07-14 14:30:41 +0530

    Again

---
+++ MyBaby/MyBaby/CodehandlerLocation.swift
@@ -0,0 +1,52 @@
+//
+//  CodehandlerLocation.swift
+//  MyBaby
+//
+//  Created by Pawan Kumar on 13/07/19.
+//  Copyright © 2019 Pawan Kumar. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreLocation
+
+public protocol MyBabyLocationDelegate {
+    func locationUpdateFetch(latitude : Double,longitude : Double)
+}
+
+public class CodeHandlerLocation : NSObject,CLLocationManagerDelegate{
+
+    var locationManager: CLLocationManager?
+    var ControllerGet : UIViewController? = nil
+
+    public func StartLocationFetch(Controller : UIViewController){
+
+        locationManager = CLLocationManager()
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.requestWhenInUseAuthorization()
+        ControllerGet = Controller
+        if CLLocationManager.locationServicesEnabled()
+        {
+
+            locationManager?.delegate = self
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager?.startUpdatingLocation()
+        }
+    }
+
+   public func StopLocationFetch(){
+        locationManager?.delegate = nil
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0]
+        let Delegate : MyBabyLocationDelegate? = ControllerGet as? MyBabyLocationDelegate
+        Delegate?.locationUpdateFetch(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+    }
+
+}
+
+
+
+
+
