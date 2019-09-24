b8c83ba407d59ed9a0e5686d6f07f948217fff1f
pawan <pawan.5340124@gmail.com>
2019-09-24 10:43:53 +0530

    raw data

--- MyBaby/MyBaby/CodeHandelerApi.swift
+++ MyBaby/MyBaby/CodeHandelerApi.swift
@@ -84,6 +84,74 @@ public class CodeHandlerApi{
     }
     
     
+    public func ApiHitUsingPostMethodRawDataFormat( APiUrl: NSString,HeaderParameter : [String: String] , BodyParameter: NSDictionary,ApiName : String,Log : Bool,Controller : UIViewController) {
+        
+        let Delegate : ApiResponceDelegateMB? = Controller as? ApiResponceDelegateMB
+        var url = APiUrl
+        url = url.replacingOccurrences(of: " ", with: "%20") as NSString
+        URLCache.shared.removeAllCachedResponses()
+        
+        //Create a non-caching configuration.
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        config.urlCache = nil
+        var  jsonData = NSData()
+        do {
+            jsonData = try JSONSerialization.data(withJSONObject: Body, options: .prettyPrinted) as NSData
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        Alamofire.request(url as String , method: .post, parameters: jsonData as? Parameters,headers:HeaderParameter).responseJSON { response in
+            
+            if Log == true{
+                print("API NAME :-  \(ApiName)")
+                print("API URL :-  \(APiUrl)")
+                print("API HeaderParameter :-  \(HeaderParameter)")
+                print("API BodyParameter :-  \(BodyParameter)")
+                print("API Result :-  \(response)")
+            }
+            
+            let InternetCheck = NetworkReachabilityManager()!.isReachable
+            if InternetCheck == false{
+                let JSON = ["message":"Please check internet connection.","ApiName":ApiName,"status":"001"]
+                Delegate?.ApiResponceFailure(Failure: JSON as NSDictionary)
+            }
+            else{
+                
+                let statusCode = (response.response?.statusCode) ?? 0 //example : 200
+                
+                if statusCode == 200{
+                    if let JSON = response.result.value {
+                        let sessionExpireJson = JSON as! NSDictionary
+                        let GetAllApiData : NSMutableDictionary = NSMutableDictionary.init(dictionary: sessionExpireJson)
+                        GetAllApiData.setValue(ApiName, forKey: "ApiName")
+                        Delegate?.ApiResponceSuccess(Success: GetAllApiData)
+                    }
+                    else{
+                        let JSON = ["message":"Due to some reason error occur please try again","ApiName":ApiName,"status":"002"]
+                        Delegate?.ApiResponceFailure(Failure: JSON as NSDictionary)
+                    }
+                }
+                else{
+                    if let JSON = response.result.value {
+                        let sessionExpireJson = JSON as! NSDictionary
+                        let GetAllApiData : NSMutableDictionary = NSMutableDictionary.init(dictionary: sessionExpireJson)
+                        GetAllApiData.setValue(ApiName, forKey: "ApiName")
+                        Delegate?.ApiResponceFailure(Failure: GetAllApiData as NSDictionary)
+                    }
+                    else{
+                        let JSON = ["message":"Due to some reason error occur please try again","ApiName":ApiName,"status":"002"]
+                        Delegate?.ApiResponceFailure(Failure: JSON as NSDictionary)
+                    }
+                }
+                
+                
+            }
+        }
+    }
+    
+    
     
     public func ApiHitUsingGetMethod( APiUrl: NSString,HeaderParameter : [String: String] , BodyParameter: NSDictionary,ApiName : String,Log : Bool,Controller : UIViewController) {
 
